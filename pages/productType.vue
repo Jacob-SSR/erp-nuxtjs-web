11f84b6d3931d492c5013424c75ed44df91ad0b6
Iam-Jacobbbbbbb <teepakorn.snis@gmail.com>
2025-07-15 01:37:06 +0700

    product type ui

--- pages/productType.vue
+++ pages/productType.vue
@@ -1,12 +1,42 @@
 <script setup>
+import config from "~/config";
+import Swal from "sweetalert2";
+
 const showModal = ref(false);
 
 definePageMeta({
   layout: "admin",
 });
 
+const name = ref("");
+const remark = ref("");
+
 const closeModal = () => {
   showModal.value = false;
+};
+
+const save = async () => {
+  try {
+    const payload = {
+      name: name.value,
+      remark: remark.value,
+    };
+
+    const { data, error } = await useFetch(
+      `${config.apiServer}/api/productType/create`,
+      {
+        method: "POST",
+        body: payload,
+        server: false,
+      }
+    );
+  } catch (error) {
+    Swal.fire({
+      icon: "error",
+      title: "Error",
+      text: error.message,
+    });
+  }
 };
 </script>
 
@@ -21,11 +51,11 @@ const closeModal = () => {
 
   <Modal v-if="showModal" title="เพิ่มประเภทสินค้า" @close="closeModal">
     <div>ชื่อ</div>
-    <input type="text" class="form-control" />
+    <input type="text" class="form-control" v-model="name" />
     <div class="mt-3">หมายเหตุ</div>
-    <input type="text" class="form-control" />
+    <input type="text" class="form-control" v-model="remark" />
 
-    <button class="btn btn-primary mt-3 cursor-pointer">
+    <button class="btn btn-primary mt-3 cursor-pointer" @click="save">
       <i class="fa fa-check me-2"></i>
       บันทึก
     </button>
