63f4b2a3f6893f7089796e634aed8d91f8c55c06
Iam-Jacobbbbbbb <teepakorn.snis@gmail.com>
2025-07-15 14:58:49 +0700

    faetuer/productType success

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -3,8 +3,8 @@
 <template>
   <div class="flex">
     <div class="sidebar"><Sidebar /></div>
-    <div class="p-4 w-full bg-gray-300">
-      <div class="bg-white p-4 rounded-xl shadow-xl">
+    <div class="p-4 w-full bg-gradient-to-b from-gray-500 bg-gray-300">
+      <div class="bg-white p-4 rounded-xl shadow-lg shadow-gray-500">
         <slot />
       </div>
     </div>
--- pages/productType.vue
+++ pages/productType.vue
@@ -10,9 +10,41 @@ definePageMeta({
 
 const name = ref("");
 const remark = ref("");
+const id = ref("");
+const productTypes = ref([]);
 
 const closeModal = () => {
   showModal.value = false;
+};
+
+onMounted(async () => {
+  await fetchData();
+});
+
+const fetchData = async () => {
+  try {
+    const { data, error } = await useFetch(
+      `${config.apiServer}/api/productType/list`,
+      {
+        method: "GET",
+      }
+    );
+    if (error.value) {
+      return Swal.fire({
+        icon: "error",
+        title: "Save Failed",
+        text: "Please try again.",
+      });
+    } else {
+      productTypes.value = data.value?.results || [];
+    }
+  } catch (error) {
+    Swal.fire({
+      icon: "error",
+      title: "Error",
+      text: error.message,
+    });
+  }
 };
 
 const save = async () => {
@@ -22,14 +54,67 @@ const save = async () => {
       remark: remark.value,
     };
 
-    const { data, error } = await useFetch(
-      `${config.apiServer}/api/productType/create`,
-      {
-        method: "POST",
-        body: payload,
+    let url = "";
+    let method = "";
+
+    if (id.value === "") {
+      url = `${config.apiServer}/api/productType/create`;
+      method = "POST";
+    } else {
+      url = `${config.apiServer}/api/productType/update/${id.value}`;
+      method = "PUT";
+    }
+
+    const { data, error } = await useFetch(url, {
+      method,
+      body: payload,
+      server: false,
+    });
+
+    if (error.value) {
+      return Swal.fire({
+        icon: "error",
+        title: "Save Failed",
+        text: error.value?.message || "Please try again.",
+      });
+    }
+
+    id.value = "";
+    await fetchData();
+    closeModal();
+  } catch (error) {
+    Swal.fire({
+      icon: "error",
+      title: "Error",
+      text: error.message,
+    });
+  }
+};
+
+const update = (productTypes) => {
+  id.value = productTypes.id;
+  name.value = productTypes.name;
+  remark.value = productTypes.remark;
+  showModal.value = true;
+};
+
+const remove = async (id) => {
+  try {
+    const button = await Swal.fire({
+      icon: "warning",
+      title: "ยืนยันการลบ",
+      text: "คุณต้องการลบประเภทสินค้านี้หรือไหม?",
+      showCancelButton: true,
+      showConfirmButton: true,
+    });
+
+    if (button.isConfirmed) {
+      await useFetch(`${config.apiServer}/api/productType/remove/${id}`, {
+        method: "DELETE",
         server: false,
-      }
-    );
+      });
+      await fetchData();
+    }
   } catch (error) {
     Swal.fire({
       icon: "error",
@@ -47,6 +132,30 @@ const save = async () => {
       <i class="fa fa-plus"></i>
       เพิ่มประเภทสินค้า
     </button>
+
+    <table class="table table-bordered mt-3">
+      <thead>
+        <tr>
+          <th>ชื่อ</th>
+          <th>หมายเหตุ</th>
+          <th width="110px"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="productType in productTypes" :key="productType.id">
+          <td>{{ productType.name }}</td>
+          <td>{{ productType.remark }}</td>
+          <td class="text-center">
+            <button class="btn btn-primary me-1" @click="update(productType)">
+              <i class="fa fa-pencil"></i>
+            </button>
+            <button class="btn btn-danger" @click="remove(productType.id)">
+              <i class="fa fa-trash"></i>
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 
   <Modal v-if="showModal" title="เพิ่มประเภทสินค้า" @close="closeModal">
